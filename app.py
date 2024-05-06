e4b3f28d30aee6efa9f3d75995b62c411690294c
Yashraj Jyoti Sanjay Kastode <yashrajkastode@gmail.com>
2024-05-06 22:01:16 +0530

    added few files

---
+++ app.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from keras.models import load_model
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+model_options = ['AAPL', 'GOOG', 'TSLA']
+stock_symbol = st.selectbox('Select Stock Symbol', model_options)
+model_path_AAPL = 'D:/Ambitions/Winter 24/PE1/Project/Keras Files/AAPL.keras'
+model_path_GOOG = 'D:/Ambitions/Winter 24/PE1/Project/Keras Files/GOOG.keras'
+model_path_TSLA = 'D:/Ambitions/Winter 24/PE1/Project/Keras Files/TSLA.keras'
+
+
+
+#new_var = st.selectbox('Select Stock Symbol', model_options)
+#selected_model = model_options[new_var]
+
+if stock_symbol == "AAPL":
+    model = load_model(model_path_AAPL)
+elif stock_symbol == "GOOG":
+    model = load_model(model_path_GOOG)
+elif stock_symbol == "TSLA":
+    model = load_model(model_path_TSLA)
+
+
+#model = load_model(selected_model)
+
+st.header('Stock Market Predictor')
+
+start = '2010-01-01'
+end = '2023-12-31'
+
+data = yf.download(stock_symbol, start ,end)
+
+st.subheader('Stock Data')
+st.write(data)
+
+data_train = pd.DataFrame(data.Close[0: int(len(data)*0.80)])
+data_test = pd.DataFrame(data.Close[int(len(data)*0.80): len(data)])
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range=(0,1))
+
+pas_100_days = data_train.tail(100)
+data_test = pd.concat([pas_100_days, data_test], ignore_index=True)
+data_test_scale = scaler.fit_transform(data_test)
+
+st.subheader('Price vs MA50')
+ma_50_days = data.Close.rolling(50).mean()
+fig1 = plt.figure(figsize=(8,6))
+plt.plot(ma_50_days, 'r')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig1)
+
+st.subheader('Price vs MA50 vs MA100')
+ma_100_days = data.Close.rolling(100).mean()
+fig2 = plt.figure(figsize=(8,6))
+plt.plot(ma_50_days, 'r')
+plt.plot(ma_100_days, 'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig2)
+
+st.subheader('Price vs MA100 vs MA200')
+ma_200_days = data.Close.rolling(200).mean()
+fig3 = plt.figure(figsize=(8,6))
+plt.plot(ma_100_days, 'r')
+plt.plot(ma_200_days, 'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig3)
+
+x = []
+y = []
+
+for i in range(100, data_test_scale.shape[0]):
+    x.append(data_test_scale[i-100:i])
+    y.append(data_test_scale[i,0])
+
+x,y = np.array(x), np.array(y)
+
+predict = model.predict(x)
+
+scale = 1/scaler.scale_
+
+predict = predict * scale
+y = y * scale
+
+st.subheader('Original Price vs Predicted Price')
+fig4 = plt.figure(figsize=(8,6))
+plt.plot(predict, 'r', label='Original Price')
+plt.plot(y, 'g', label = 'Predicted Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.show()
+st.pyplot(fig4)
